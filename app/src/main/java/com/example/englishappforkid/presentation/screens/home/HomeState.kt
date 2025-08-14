28014a4414031fef510603dcfbaa453c30c80acc
HOANGNAM1807 <166529296+HOANGNAM1807@users.noreply.github.com>
2025-08-14 08:24:51 +0700

    feat: Implement functional skeleton for content list screen (#4)

---
+++ app/src/main/java/com/example/englishappforkid/presentation/screens/home/HomeState.kt
@@ -0,0 +1,8 @@
+package com.example.englishappforkid.presentation.screens.home
+
+import com.example.englishappforkid.data.model.Topic
+
+data class HomeState(
+    val topics: List<Topic> = emptyList(),
+    val isListView: Boolean = true
+)
